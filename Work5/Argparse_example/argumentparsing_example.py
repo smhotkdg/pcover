7e1061034bf42da7f9e1e948cf896fa093eb8ed2
JongHyeok Lee <ljh860607@naver.com>
2014-04-12 18:58:42 +0900

    Input two integer values then print

---
+++ Work5/Argparse_example/argumentparsing_example.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+argumentparsing 예제
+문제는 현재 if arg.Width...에서 0 or 나머지 정수값으로 true false결정->"" (NULL, None) 사용
+
+'''
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument("--Width",help="Width value of Cover image", type=int)
+parser.add_argument("--Height",help="Height value of Cover image", type=int)
+args=parser.parse_args()
+
+if args.Width!="":
+	print('Width:',args.Width)
+if args.Height!="":
+	print('Height:',args.Height)
