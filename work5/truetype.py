3e12116542ee43665dcdcc77d19119806a84a569
LEE GUKSEON <kooksuni21@gmail.com>
2014-05-14 21:34:31 +0900

    add parameter and result

--- work5/truetype.py
+++ work5/truetype.py
@@ -8,28 +8,31 @@ from PIL import Image, ImageDraw, ImageFont
 4. 타이틀 및 책 정보
 5. 클래스 또한 함수화 
 '''
-pattern_im = Image.open("pattern_image.jpg")
-#pattern_im.show()
-w,h = pattern_im.size
-bg_im = Image.new(mode='RGB', size=(w,h),color=(255,255,254))
-font = ImageFont.truetype("Arial.ttf", 200, encoding='unic')
-draw = ImageDraw.Draw(bg_im)
-line_spacing = draw.textsize('A', font=font)[1] + 50
-lines = ['Gwang-Soo Hong', 'Sunmoon Univ.', 'Dept. of Computer Science and Engineering']
-y = 0
-for line in lines:
-    draw.text((0, y), line, font=font)
-    y += line_spacing
-#bg_im.save('test.png', 'PNG')
-for j in range(0,h):
-    for i in range(0,w):
-        bg_r, bg_g, bg_b = bg_im.getpixel((i,j))
-        #print (r,g,b)
-        if (bg_r,bg_g,bg_b) != (255,255,254):
-            #print (bg_r,bg_b,bg_b)
-            pattern_r, pattern_g, pattern_b = pattern_im.getpixel((i,j))
-            bg_im.putpixel((i,j),(pattern_r,pattern_g,pattern_b))                
-        else:
-            pass
-bg_im.save("test.png","PNG")
+def makingCover(pattern_image, fonttype = 'arial.ttf', string = 'X', color = (231,12,39), width = 450, height = 800):
+    pattern_im = Image.open(str(pattern_image))
+    #pattern_im.show()
+    #w,h = pattern_im.size
+    bg_im = Image.new(mode='RGB', size=(width,height),color=color)
+    font = ImageFont.truetype(str(fonttype), width, encoding='unic')
+    draw = ImageDraw.Draw(bg_im)
+    x,y = font.getsize(string)
+    #line_spacing = draw.textsize('A', font=font)[1] + 50
+    #lines = ['Gwang-Soo Hong', 'Sunmoon Univ.', 'Dept. of Computer Science and Engineering']
+    #y = 0
+    #for line in lines:
+    #    draw.text((0, y), line, font=font)
+    #    y += line_spacing
+    draw.text((int((width-x)/2),int(height/8)), string,font=font)
+    #bg_im.save('test.png', 'PNG')
+    for j in range(0,height):
+        for i in range(0,width):
+            bg_r, bg_g, bg_b = bg_im.getpixel((i,j))
+            #print (r,g,b)
+            if (bg_r,bg_g,bg_b) != color:
+                #print (bg_r,bg_b,bg_b)
+                pattern_r, pattern_g, pattern_b = pattern_im.getpixel((i,j))
+                bg_im.putpixel((i,j),(pattern_r,pattern_g,pattern_b))                
+            else:
+                pass
+    bg_im.save("test.png","PNG")
                            
